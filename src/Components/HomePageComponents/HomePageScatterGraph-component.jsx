066a986cb5b673faac2d74a5a7b4806d98baa691
RobJ <robjjohnsenwork@gmail.com>
2022-12-28 18:13:38 +0700

    Styled and fixed the scatter graph initial problems

--- src/Components/HomePageComponents/HomePageScatterGraph-component.jsx
+++ src/Components/HomePageComponents/HomePageScatterGraph-component.jsx
@@ -1,35 +1,65 @@
 import React from "react";
-import { ScatterChart, Scatter, XAxis, YAxis, Tooltip, Label } from "recharts";
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
 
 const data = [
-  { x: 20, y: 80 },
-  { x: 40, y: 60 },
-  { x: 60, y: 70 },
-  { x: 80, y: 50 },
-  { x: 100, y: 40 },
+  { x: 10, y: 20, z: "boob" },
+  { x: 20, y: 80, z: "qq" },
+  { x: 40, y: 60, z: "ww" },
+  { x: 60, y: 70, z: "ee" },
+  { x: 80, y: 50, z: "rr" },
+  { x: 100, y: 40, z: "tt" },
 ];
 
 const ScatterGraph = () => {
   return (
-    <ScatterChart width={600} height={500} data={data}>
-      <XAxis
-        dataKey="x"
-        label={{
-          value: "Environmental score",
-          position: "insideBottom",
-          offset: -5,
-        }}
-      />
+    <div className="flex flex-col gap-2 h-full w-full items-center">
+      <h2 className="underline">Scatter Graph Vietnam</h2>
 
-      <YAxis
-        label={{ value: "Economic Score", angle: -90, position: "insideLeft" }}
-        dataKey="y"
-        ticks={[0, 20, 40, 60, 80, 100]}
-      />
+      <ResponsiveContainer className="w-full h-full">
+        <ScatterChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 15,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            label={{
+              value: "Environmental score",
+              position: "insideBottom",
+              offset: -10,
+            }}
+            type="number"
+            dataKey="x"
+            ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+          />
 
-      <Tooltip />
-      <Scatter dataKey="y" fill="#8884d8" />
-    </ScatterChart>
+          <YAxis
+            label={{
+              value: "Economic Score",
+              angle: -90,
+              position: "insideLeft",
+            }}
+            dataKey="y"
+            ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+          />
+
+          <Tooltip />
+          <Scatter fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
