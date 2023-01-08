5c65c473530e155878389016aabb6f628bc76b85
RobJ <robjjohnsenwork@gmail.com>
2023-01-08 13:25:32 +0700

    Added custom scrolling class. Hides scroll so that sizing if not affected, but keeps functionality

--- src/Components/HomePageComponents/ListSection-component.jsx
+++ src/Components/HomePageComponents/ListSection-component.jsx
@@ -11,14 +11,26 @@ function ListSection(params) {
         <div className="w-1/2">Search bar</div>
       </section>
       {/* TABLE DISTRICT SECTION */}
-      <section className="w-full h-full bg-red-300 flex flex-col">
-        <div className="w-full flex text-center border-t-2 border-b-2 border-black">
+      <section className="w-full h-full bg-red-300 flex flex-col overflow-auto">
+        <div className="w-full flex text-center border-t-2 border-b-2 border-black ">
           <span className="w-1/12  border-r-2 border-black">No</span>
           <span className="w-3/12  border-r-2 border-black">District</span>
           <span className="w-4/12 border-r-2 border-black">Economic</span>
           <span className="w-4/12 ">Environmental</span>
         </div>
-        <div className="w-full h-full ">
+        <div className="w-full h-full overflow-auto scrollbar-none">
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
+          <ListSectionItem />
           <ListSectionItem />
         </div>
       </section>
