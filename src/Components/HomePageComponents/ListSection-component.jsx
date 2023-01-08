1fceafe251cdd63c1d5d0ef08c3843345f3478a0
RobJ <robjjohnsenwork@gmail.com>
2023-01-08 12:09:04 +0700

    building out listSection, child component item

--- src/Components/HomePageComponents/ListSection-component.jsx
+++ src/Components/HomePageComponents/ListSection-component.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { FaMapMarkedAlt } from "react-icons/fa";
+import ListSectionItem from "./ListSectionItem-component";
 //
 //
 function ListSection(params) {
   return (
-    <div className="bg-red-100 w-full h-full flex flex-col">
+    <div className=" w-full h-full flex flex-col">
       <section className="w-full bg-red-200 flex">
         <div className="w-1/2">Vietnam : Districts : 2021 </div>
         <div className="w-1/2">Search bar</div>
       </section>
-      <section className="w-full h-full">Data</section>
+      {/* TABLE DISTRICT SECTION */}
+      <section className="w-full h-full bg-red-300 flex flex-col">
+        <div className="w-full flex text-center border-t-2 border-b-2 border-black">
+          <span className="w-1/12  border-r-2 border-black">No</span>
+          <span className="w-3/12  border-r-2 border-black">District</span>
+          <span className="w-4/12 border-r-2 border-black">Economic</span>
+          <span className="w-4/12 ">Environmental</span>
+        </div>
+        <div className="w-full h-full ">
+          <ListSectionItem />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/Components/HomePageComponents/ListSectionItem-component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//
+//
+function ListSectionItem(params) {
+  return (
+    <div className="w-full flex  text-center border-b-2 border-slate-300">
+      <span className="w-1/12 border-r-2 border-slate-300">1</span>
+      <span className="w-3/12 border-r-2 border-slate-300">An Giang</span>
+      <span className="w-4/12 border-r-2 border-slate-300">Econ bar</span>
+      <span className="w-4/12 ">Env bar</span>
+    </div>
+  );
+}
+//
+export default ListSectionItem;
